13f4ecdadc1369ca7a49830f6beea3c5c8adafba
Denis <denzh@mail.ru>
2023-07-28 05:32:44 +0500

    picocli: options and parameters setup

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,50 @@
 package hexlet.code;
 
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+import java.io.File;
+import java.math.BigInteger;
+import java.nio.file.Files;
+import java.security.MessageDigest;
+import java.util.concurrent.Callable;
+
+@Command(
+        version = "Help demo v0.1",
+        name = "gendiff",
+        description = "Compares two configuration files and shows a difference."
+)
+
+public class App implements Runnable {
+    @Parameters(index = "0", description = "path to first file", paramLabel = "filepath1")
+    private File file1;
+
+    @Parameters(index = "1", description = "path to second file", paramLabel = "filepath2")
+    private File file2;
+
+    @Option(names = { "-f", "--format"}, description = "output format [default: stylish]", paramLabel = "format")
+    File file;
+
+    @Option(names = {"-V", "--version"}, description = "Print version information and exit.", versionHelp = true)
+    boolean versionHelpRequested;
+
+    @Option(names = {"-h", "--help"}, description = "Show this help message and exit.", usageHelp = true)
+    boolean usageHelpRequested;
+
+    @Override
+    public void run() {
+
     }
+
+    public static void main(String[] args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
+//        System.out.println("Hello world!");
+    }
+
+
 }
+
+
